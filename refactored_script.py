b1a8b377e8c38d6931c6be1cc43f637eb1df8184
CalebMartin10 <Martinc10@csp.edu>
2025-10-17 22:16:42 -0500

    Changes: Tweaked the mixed_case_concatenation doc strings grammer. Made the doc strings double instead of single quotes.

--- refactored_script.py
+++ refactored_script.py
@@ -1,5 +1,5 @@
 def parity_multiplication(list_of_nums):
-    '''
+    """
     Multiply values in a list of numbers based on their parity.
 
     Args:
@@ -7,19 +7,19 @@ def parity_multiplication(list_of_nums):
 
     Returns:
     result(list): A list containing multiplied numbers.
-    '''
+    """
     return [num * 2 if num % 2 == 0 else num * 3 for num in list_of_nums]  # Checks the remainder to see if the number is even or odd
 
 def mixed_case_concatenation(list_of_words):
-    '''
-    Make a mixed case sentence where words length five or more are upper case and rest are lower case.
+    """
+    Make a mixed case sentence where words length five or more are upper case and the rest are lower case.
 
     Args:
     list_of_words(list): A list containing words/strings.
 
     Returns:
     phrase.strip(string): A long string which is a large mixed case sentence.
-    '''
+    """
     phrase = ""
     try:
         new_list = [word.upper() + " " if len(word) > 5 else word.lower() + " " for word in list_of_words]  # Makes words all upper or lower case based on their length
@@ -31,12 +31,12 @@ def mixed_case_concatenation(list_of_words):
 
 
 def script_logic():
-    '''
+    """
     Test the functions parity_multiplication and mixed_case_concatenation.
 
     Retruns:
     Two print statements.
-    '''
+    """
     number_list = [1, 2, 3, 4, 5, 6, 7]
     fruit_list = ["apple", "banana", "kiwi", "grapefruit", "cherry"]
 
